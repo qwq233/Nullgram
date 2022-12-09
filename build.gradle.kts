daad1470e9d9dca4c0e668a5b14c46cbde602421
qwq233 <qwq233@qwq2333.top>
2022-12-10 01:05:49 +0800

    misc: bump agp version to v7.3.1
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 @file:Suppress("UnstableApiUsage")
 
 plugins {
-    id("com.android.application") version "7.2.2" apply false
-    id("com.android.library") version "7.2.2" apply false
+    id("com.android.application") version "7.3.1" apply false
+    id("com.android.library") version "7.3.1" apply false
     id("com.google.gms.google-services") version "4.3.13" apply false
     id("org.jetbrains.kotlin.android") version Version.kotlin apply false
     kotlin("plugin.serialization") version Version.kotlin apply false
