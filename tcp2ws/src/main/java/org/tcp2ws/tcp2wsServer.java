de66f60dc448ea4b3e182ef9d2a7a4d854dc4688
Naomi <mangogo.naomi@qq.com>
2023-04-11 00:03:44 +0800

    feat: update tcp2wsServer
    Signed-off-by: Naomi <mangogo.naomi@qq.com>

--- tcp2ws/src/main/java/org/tcp2ws/tcp2wsServer.java
+++ tcp2ws/src/main/java/org/tcp2ws/tcp2wsServer.java
@@ -1,5 +1,7 @@
 package org.tcp2ws;
 
+import com.neovisionaries.ws.client.WebSocket;
+
 import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.net.InetAddress;
@@ -7,6 +9,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class tcp2wsServer {
@@ -19,74 +22,86 @@ public class tcp2wsServer {
 
     static Map<String, String> cdn = new HashMap<>();
     static Map<Integer, String> mtpcdn = new HashMap<>();
+    static final Map<String, HashSet<WebSocket>> inactiveWs = new HashMap<>();
 
-    static {
-        mtpcdn.put(1, "pluto.nekoe.eu.org");
-        mtpcdn.put(2, "venus.nekoe.eu.org");
-        mtpcdn.put(3, "aurora.nekoe.eu.org");
-        mtpcdn.put(4, "vesta.nekoe.eu.org");
-        mtpcdn.put(5, "flora.nekoe.eu.org");
-        mtpcdn.put(17, "test_pluto.nekoe.eu.org");
-        mtpcdn.put(18, "test_venus.nekoe.eu.org");
-        mtpcdn.put(19, "test_aurora.nekoe.eu.org");
+    public tcp2wsServer setCdnDomain(String domain) {
+        mtpcdn.put(1, "pluto." + domain);
+        mtpcdn.put(2, "venus." + domain);
+        mtpcdn.put(3, "aurora." + domain);
+        mtpcdn.put(4, "vesta." + domain);
+        mtpcdn.put(5, "flora." + domain);
+        mtpcdn.put(17, "test_pluto." + domain);
+        mtpcdn.put(18, "test_venus." + domain);
+        mtpcdn.put(19, "test_aurora." + domain);
 
 //media
-        cdn.put("149.154.175.50", "pluto.nekoe.eu.org");
-        cdn.put("149.154.167.51", "venus.nekoe.eu.org");
-        cdn.put("95.161.76.100", "venus.nekoe.eu.org");
-        cdn.put("149.154.175.100", "aurora.nekoe.eu.org");
-        cdn.put("149.154.167.91", "vesta.nekoe.eu.org");
-        cdn.put("149.154.171.5", "flora.nekoe.eu.org");
+        cdn.put("149.154.175.50", "pluto." + domain);
+        cdn.put("149.154.167.51", "venus." + domain);
+        cdn.put("95.161.76.100", "venus." + domain);
+        cdn.put("149.154.175.100", "aurora." + domain);
+        cdn.put("149.154.167.91", "vesta." + domain);
+        cdn.put("149.154.171.5", "flora." + domain);
 
         try {
-            cdn.put(InetAddress.getByName("2001:b28:f23d:f001:0000:0000:0000:000a").getHostAddress(), "pluto.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:67c:4e8:f002:0000:0000:0000:000a").getHostAddress(), "venus.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:b28:f23d:f003:0000:0000:0000:000a").getHostAddress(), "aurora.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:67c:4e8:f004:0000:0000:0000:000a").getHostAddress(), "vesta.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:b28:f23f:f005:0000:0000:0000:000a").getHostAddress(), "flora.nekoe.eu.org");
+            cdn.put(InetAddress.getByName("2001:b28:f23d:f001:0000:0000:0000:000a").getHostAddress(), "pluto." + domain);
+            cdn.put(InetAddress.getByName("2001:67c:4e8:f002:0000:0000:0000:000a").getHostAddress(), "venus." + domain);
+            cdn.put(InetAddress.getByName("2001:b28:f23d:f003:0000:0000:0000:000a").getHostAddress(), "aurora." + domain);
+            cdn.put(InetAddress.getByName("2001:67c:4e8:f004:0000:0000:0000:000a").getHostAddress(), "vesta." + domain);
+            cdn.put(InetAddress.getByName("2001:b28:f23f:f005:0000:0000:0000:000a").getHostAddress(), "flora." + domain);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
 //proxy
-        cdn.put("149.154.175.5", "pluto.nekoe.eu.org");
-        cdn.put("149.154.161.144", "venus.nekoe.eu.org");
-        cdn.put("149.154.167.15", "venus.nekoe.eu.org");
-        cdn.put("149.154.167.5", "venus.nekoe.eu.org");
-        cdn.put("149.154.167.6", "venus.nekoe.eu.org");
-        cdn.put("149.154.167.7", "venus.nekoe.eu.org");
-        cdn.put("149.154.167.2", "venus.nekoe.eu.org");
-        cdn.put("91.108.4.", "vesta.nekoe.eu.org");
-        cdn.put("149.154.164.", "vesta.nekoe.eu.org");
-        cdn.put("149.154.165.", "vesta.nekoe.eu.org");
-        cdn.put("149.154.166.", "vesta.nekoe.eu.org");
-        cdn.put("149.154.167.8", "vesta.nekoe.eu.org");
-        cdn.put("149.154.167.9", "vesta.nekoe.eu.org");
-        cdn.put("91.108.56.", "flora.nekoe.eu.org");
-        cdn.put("111.62.91.", "venus.nekoe.eu.org");
+        cdn.put("149.154.175.5", "pluto." + domain);
+        cdn.put("149.154.161.144", "venus." + domain);
+        cdn.put("149.154.167.15", "venus." + domain);
+        cdn.put("149.154.167.5", "venus." + domain);
+        cdn.put("149.154.167.6", "venus." + domain);
+        cdn.put("149.154.167.7", "venus." + domain);
+        cdn.put("149.154.167.2", "venus." + domain);
+        cdn.put("91.108.4.", "vesta." + domain);
+        cdn.put("149.154.164.", "vesta." + domain);
+        cdn.put("149.154.165.", "vesta." + domain);
+        cdn.put("149.154.166.", "vesta." + domain);
+        cdn.put("149.154.167.8", "vesta." + domain);
+        cdn.put("149.154.167.9", "vesta." + domain);
+        cdn.put("91.108.56.", "flora." + domain);
+        cdn.put("111.62.91.", "venus." + domain);
 
         try {
-            cdn.put(InetAddress.getByName("2001:b28:f23d:f001:0000:0000:0000:000d").getHostAddress(), "pluto.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:67c:4e8:f002:0000:0000:0000:000d").getHostAddress(), "venus.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:b28:f23d:f003:0000:0000:0000:000d").getHostAddress(), "aurora.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:67c:4e8:f004:0000:0000:0000:000d").getHostAddress(), "vesta.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:b28:f23f:f005:0000:0000:0000:000d").getHostAddress(), "flora.nekoe.eu.org");
+            cdn.put(InetAddress.getByName("2001:b28:f23d:f001:0000:0000:0000:000d").getHostAddress(), "pluto." + domain);
+            cdn.put(InetAddress.getByName("2001:67c:4e8:f002:0000:0000:0000:000d").getHostAddress(), "venus." + domain);
+            cdn.put(InetAddress.getByName("2001:b28:f23d:f003:0000:0000:0000:000d").getHostAddress(), "aurora." + domain);
+            cdn.put(InetAddress.getByName("2001:67c:4e8:f004:0000:0000:0000:000d").getHostAddress(), "vesta." + domain);
+            cdn.put(InetAddress.getByName("2001:b28:f23f:f005:0000:0000:0000:000d").getHostAddress(), "flora." + domain);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
 
 //test
-        cdn.put("149.154.175.10", "test_pluto.nekoe.eu.org");
-        cdn.put("149.154.175.40", "test_pluto.nekoe.eu.org");
-        cdn.put("149.154.167.40", "test_venus.nekoe.eu.org");
-        cdn.put("149.154.175.117", "test_aurora.nekoe.eu.org");
+        cdn.put("149.154.175.10", "test_pluto." + domain);
+        cdn.put("149.154.175.40", "test_pluto." + domain);
+        cdn.put("149.154.167.40", "test_venus." + domain);
+        cdn.put("149.154.175.117", "test_aurora." + domain);
 
         try {
-            cdn.put(InetAddress.getByName("2001:b28:f23d:f001:0000:0000:0000:000e").getHostAddress(), "test_pluto.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:67c:4e8:f002:0000:0000:0000:000e").getHostAddress(), "test_venus.nekoe.eu.org");
-            cdn.put(InetAddress.getByName("2001:b28:f23d:f003:0000:0000:0000:000e").getHostAddress(), "test_aurora.nekoe.eu.org");
+            cdn.put(InetAddress.getByName("2001:b28:f23d:f001:0000:0000:0000:000e").getHostAddress(), "test_pluto." + domain);
+            cdn.put(InetAddress.getByName("2001:67c:4e8:f002:0000:0000:0000:000e").getHostAddress(), "test_venus." + domain);
+            cdn.put(InetAddress.getByName("2001:b28:f23d:f003:0000:0000:0000:000e").getHostAddress(), "test_aurora." + domain);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
+
+        inactiveWs.put(tcp2wsServer.mtpcdn.get(1), new HashSet<>());
+        inactiveWs.put(tcp2wsServer.mtpcdn.get(2), new HashSet<>());
+        inactiveWs.put(tcp2wsServer.mtpcdn.get(3), new HashSet<>());
+        inactiveWs.put(tcp2wsServer.mtpcdn.get(4), new HashSet<>());
+        inactiveWs.put(tcp2wsServer.mtpcdn.get(5), new HashSet<>());
+        inactiveWs.put(tcp2wsServer.mtpcdn.get(17), new HashSet<>());
+        inactiveWs.put(tcp2wsServer.mtpcdn.get(18), new HashSet<>());
+        inactiveWs.put(tcp2wsServer.mtpcdn.get(19), new HashSet<>());
+
+        return this;
     }
 
     public tcp2wsServer setUserAgent(String userAgent) {
