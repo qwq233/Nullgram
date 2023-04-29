e279ebe3e9ad1c48aff516974825c218e7ba990b
qwq233 <qwq233@qwq2333.top>
2023-04-29 22:06:27 +0800

    fix: ci
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,19 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+        publishOnFailure()
+    }
+}
+
 rootProject.name = "Nullgram"
 include(
     ":TMessagesProj",
