f7a4c0b822980c4d438eb62289b86bd9a3374393
qwq233 <qwq233@qwq2333.top>
2023-07-01 00:39:51 +0800

    chore: ensure some crucial value would not be changed accidentally
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -18,24 +18,23 @@ import java.util.Objects;
 
 public class BuildVars {
 
-    public static boolean DEBUG_VERSION = true;
+    public static final boolean DEBUG_VERSION = true;
     public static boolean DEBUG_PRIVATE_VERSION = false;
     public static boolean CHECK_UPDATES = true;
     public static boolean LOGS_ENABLED = true;
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
-    public static int BUILD_VERSION = 3362;
-    public static String BUILD_VERSION_STRING = "9.6.6";
-    public static int APP_ID = 19797609;
-    public static String APP_HASH = "e8f1567dbbf38944a1391c4d23c34b60";
-    public static String APPCENTER_HASH = "e07b49da-11a5-46db-a780-f5cd7b9a1a5a";
+    public static final int BUILD_VERSION = 3362;
+    public static final String BUILD_VERSION_STRING = "9.6.6";
+    public static final int APP_ID = 19797609;
+    public static final String APP_HASH = "e8f1567dbbf38944a1391c4d23c34b60";
+    public static final String APPCENTER_HASH = "e07b49da-11a5-46db-a780-f5cd7b9a1a5a";
     public static String SAFETYNET_KEY = "";
 
     public static String SMS_HASH = "O2P2z+/jBpJ";
-    public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=top.qwq2333.nullgram";
+    public static final  String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=top.qwq2333.nullgram";
     public static String GOOGLE_AUTH_CLIENT_ID = "760348033671-81kmi3pi84p11ub8hp9a1funsv0rn2p9.apps.googleusercontent.com";
 
-    public static String HUAWEI_APP_ID = "101184875";
 
     // You can use this flag to disable Google Play Billing (If you're making fork and want it to be in Google Play)
     public static boolean IS_BILLING_UNAVAILABLE = BuildConfig.isPlay;
