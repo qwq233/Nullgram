ef6be580ee60e0d566c2daf4e498bad7bb72871b
Ketal <41381927+keta1@users.noreply.github.com>
2023-08-17 07:32:37 +0800

    build: migrate to version catalogs (#146)
    * build: migrate to version catalogs
    update gradle
    remove multiDex
    
    * Update Version.kt
    
    Signed-off-by: James Clef <qwq233@qwq2333.top>
    
    ---------
    
    Signed-off-by: James Clef <qwq233@qwq2333.top>
    Co-authored-by: James Clef <qwq233@qwq2333.top>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 @file:Suppress("UnstableApiUsage")
 
 plugins {
-    id("com.android.application") version "8.0.2" apply false
-    id("com.android.library") version "8.0.2" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
-    id("com.google.firebase.crashlytics") version "2.9.5" apply false
-    kotlin("android") version Version.kotlin apply false
-    kotlin("plugin.serialization") version Version.kotlin apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.serialization) apply false
 }
 
 tasks.register<Delete>("clean").configure {
--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -3,17 +3,11 @@ import org.gradle.api.JavaVersion
 object Version {
 
     @JvmStatic
-    val kotlin = "1.9.0"
-
-    @JvmStatic
-    val ktor = "2.3.2"
-
-    @JvmStatic
     val java = JavaVersion.VERSION_17
 
     @JvmStatic
     val officialVersionName = "9.7.6"
 
     @JvmStatic
-    val isStable = true
+    val isStable = false
 }
