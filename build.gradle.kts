ece8be6f2761417672521d4e9a7317376a69313c
qwq233 <qwq233@qwq2333.top>
2023-09-30 16:20:06 +0800

    chore: bump target api to 34
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,10 +24,10 @@ val verName = if (Version.isStable) {
     "v" + Version.officialVersionName + "-preview-" + (Common.getGitHeadRefsSuffix(rootProject))
 }
 
-val androidTargetSdkVersion by extra(33)
+val androidTargetSdkVersion by extra(34)
 val androidMinSdkVersion by extra(24)
-val androidCompileSdkVersion by extra(33)
-val androidBuildToolsVersion by extra("33.0.2")
+val androidCompileSdkVersion by extra(34)
+val androidBuildToolsVersion by extra("34.0.0")
 val androidCompileNdkVersion = "23.2.8568313"
 
 fun Project.configureBaseExtension() {
