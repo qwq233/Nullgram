3f42bb77b46868a9a61b78c455e3bc5f728cddb6
qwq233 <qwq233@qwq2333.top>
2024-02-24 23:29:24 +0800

    feat: bump minSdk to 27
    MAJOR UPDATE
    Bump version to Android 8 (SDK 27)
    
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2019-2024 qwq233 <qwq233@qwq2333.top>
+ * https://github.com/qwq233/Nullgram
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this software.
+ *  If not, see
+ * <https://www.gnu.org/licenses/>
+ */
+
 @file:Suppress("UnstableApiUsage")
 
 plugins {
@@ -26,7 +45,7 @@ val verName = if (Version.isStable) {
 }
 
 val androidTargetSdkVersion by extra(34)
-val androidMinSdkVersion by extra(24)
+val androidMinSdkVersion by extra(27)
 val androidCompileSdkVersion by extra(34)
 val androidBuildToolsVersion by extra("34.0.0")
 val androidCompileNdkVersion = "23.2.8568313"
